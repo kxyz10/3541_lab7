376e9b6482088f1d8c6255d1148513fb32b87563
Kevin Oliver <oliver.395@osu.edu>
2021-04-15 15:39:16 -0400

    changed movement

--- Assets/Scripts/PlayerShipController.cs
+++ Assets/Scripts/PlayerShipController.cs
@@ -5,97 +5,39 @@ using UnityEngine;
 public class PlayerShipController : MonoBehaviour
 {
 
+    public float cameraSensitivity = 1.0f;
+    public float moveSpeed = 10.0f;
+    private float xRotation = 0.0f;
+    private float yRotation = 0.0f;
+    private Camera camera;
+    CharacterController shipController;
 
-    public float movementSpeed = 20f;
-    public float rotationSpeed = 10.0f;
-    // Use this for initialization
     void Start()
     {
+        camera = Camera.main;
+        shipController = GetComponent<CharacterController>();
 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(transform.right), rotationSpeed * Time.deltaTime);
-            //transform.LookAt(transform.right * Time.deltaTime);
-            //float damping = 2.0f;
-            //float rotate = Mathf.Lerp(transform.right, transform.position.y, damping * Time.deltaTime);
-            //transform.Rotate(Vector3.up * rotate);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(-transform.right), rotationSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(transform.forward,transform.up), rotationSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(transform.forward, -transform.up), rotationSpeed * Time.deltaTime);
-        }
-        //if (Input.GetKey(KeyCode.Space))
-        //{
-        //    transform.Translate(Vector3.forward * Time.deltaTime, Space.Self);
-        //}
 
-        if (Input.GetAxis("Mouse Y") < 0)
-        {
-            if (transform.position.z > -15f)
-            {
-                // moving back
-                transform.Translate(Vector3.back * movementSpeed * Time.deltaTime, Space.Self);
-            }
-        }
-        if (Input.GetAxis("Mouse Y") > 0)
-        {
-            if (transform.position.z < 30f)
-            {
-                // moving forward
-                transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime, Space.Self);
-            }
-        }
-        
-        if (Input.GetAxis("Mouse X") < 0)
-        {
-            if (transform.position.x > -30f)
-            {
-                // move left
-                transform.Translate(Vector3.left * movementSpeed * Time.deltaTime, Space.Self);
-            }
-        }
-        if (Input.GetAxis("Mouse X") > 0)
-        {
-            if (transform.position.x < 30f)
-            {
-                // move right
-                transform.Translate(Vector3.right * movementSpeed * Time.deltaTime, Space.Self);
-            }
-        }
-    }
+        // mouse movement
+        float cameraX = Input.GetAxis("Mouse X") * cameraSensitivity;
+        float cameraY = Input.GetAxis("Mouse Y") * cameraSensitivity;
+        yRotation += cameraX;
+        xRotation -= cameraY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+        camera.transform.eulerAngles = new Vector3(xRotation, yRotation, 0.0f);
 
-    Vector3 getDirection()
-    {
-        Vector3 direction = new Vector3(0,0,0);
-        if (Input.GetKey(KeyCode.W))
+        // spaceship movement
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        float forward = 0.0f;
+        if (Input.GetKey(KeyCode.Space))
         {
-            direction += Vector3.forward;
+            forward = 1;
         }
-        if (Input.GetKey(KeyCode.S))
-        {
-            direction += -Vector3.forward;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            direction += -Vector3.right;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            direction += Vector3.left;
-        }
-        return direction;
+        shipController.Move((Vector3.right * horizontal + Vector3.up * vertical + Vector3.forward * forward) * Time.deltaTime * moveSpeed);
     }
 }
